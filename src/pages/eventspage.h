1df25254f0dc82ea1ca3ce6f7ea47906874eba78
심규석 <sfpahsdev@gmail.com>
2026-08-24 01:42:07 +0900

    feat(EVDA-228): improve Events monitoring UI

--- src/pages/eventspage.h
+++ src/pages/eventspage.h
@@ -18,6 +18,7 @@ class EventsPage : public QWidget
 public:
     explicit EventsPage(QWidget *parent = nullptr);
     void appendEvent(const MonitoringEvent &event);
+    void setServerState(const QString &status, const QString &detail = QString());
 
 signals:
     void exportResult(bool success, const QString &message);
@@ -27,9 +28,12 @@ private slots:
     void applyFilters();
     void exportCsv();
     void resetFilters();
+    void updateEvidenceAction();
 
 private:
     void addFilterOption(QComboBox *comboBox, const QString &value);
+    void requestEvidenceForRow(int row);
+    void updateStatePresentation();
 
     QTableWidget *m_eventTable = nullptr;
     QLineEdit *m_searchEdit = nullptr;
@@ -37,7 +41,12 @@ private:
     QComboBox *m_eventTypeFilter = nullptr;
     QComboBox *m_statusFilter = nullptr;
     QPushButton *m_resetFilterButton = nullptr;
+    QPushButton *m_openEvidenceButton = nullptr;
     QLabel *m_filterResultLabel = nullptr;
+    QLabel *m_stateLabel = nullptr;
+    QLabel *m_selectionLabel = nullptr;
+    QString m_serverStatus;
+    QString m_serverDetail;
 };
 
 #endif
